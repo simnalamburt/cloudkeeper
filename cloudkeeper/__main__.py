3bd41ed153fc955a6c654ab08a56e3e97c77a056
Hyeon Kim <simnalamburt@gmail.com>
2017-02-23 13:50:03 +0900

    Replace print() calls with a logging module
    Closes #7

--- cloudkeeper/__main__.py
+++ cloudkeeper/__main__.py
@@ -8,12 +8,13 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import unicode_literals, absolute_import
 import sys
 import time
 import signal
 import traceback
 import binascii
+import logging
 from base64 import b64decode
 if sys.version_info >= (3,):
     import pickle
@@ -36,13 +37,16 @@ def main():
     IRCCloud server infinitely.
     '''
 
+    # Set log level
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
     # Consider SIGINT as a goodbye signal
-    handler = lambda sig, frame: (print('\n\nGoodbye!'), sys.exit(0))
+    handler = lambda sig, frame: (logging.info('Recieved SIGINT, turning off the process. Goodbye!'), sys.exit(0))
     signal.signal(signal.SIGINT, handler)
 
     # Ask credentials to the user
     if len(sys.argv) > 2:
-        print('Too many arguments have been supplied')
+        logging.error('Too many arguments have been supplied.')
         sys.exit(1)
     elif len(sys.argv) < 2:
         # No credentials supplied
@@ -54,31 +58,31 @@ def main():
             pickled = b64decode(serialized)
             data = pickle.loads(pickled)
         except (EOFError, ValueError, KeyError, binascii.Error, pickle.UnpicklingError) as e:
-            print('Wrong arguments have been supplied ({})'.format(e))
+            logging.error('Wrong arguments have been supplied. ({})'.format(e))
             sys.exit(1)
 
     # Try to connect to the server infinitely
     while True:
         connection = IRCCloud()
 
-        print('Authenticating ... ', end='')
+        logging.info('Starting authentication ...')
         sys.stdout.flush()
         result = connection.auth(*data)
         if result is None:
-            print('\x1b[31mFailed:\x1b[0m Wrong email/password combination.')
+            logging.error('Authentication failed. Perhaps wrong email/password combination.')
             sys.exit(1)
-        print('Done')
+        logging.info('Authentication completed.')
 
         try:
             connection.connect(
-                lambda: print('Connection created.'),
-                lambda: print('\x1b[33mWarning:\x1b[0m Disconnected from the network'))
+                lambda: logging.info('Connection created.'),
+                lambda: logging.warning('Disconnected from the network'))
         except SystemExit:
             raise
         except:
-            print('\x1b[31mFailed!\x1b[33m\n')
-            print(traceback.format_exc())
-            print('\x1b[0mDisconnected. Reconnecting in {} seconds.\n'.format(DELAY))
+            logging.error('Unhandled exception occurred:')
+            logging.error(traceback.format_exc())
+            logging.error('Disconnected. Reconnecting in {} seconds.'.format(DELAY))
             time.sleep(DELAY)
 
 
