af08a52d4f8ff602938539411ec6de2d16943e69
Hyeon Kim <simnalamburt@gmail.com>
2017-02-23 14:09:35 +0900

    Merge ask script and prompt.py into single one

--- cloudkeeper/prompt.py
+++ cloudkeeper/prompt.py
@@ -10,11 +10,20 @@
 
 from __future__ import print_function, unicode_literals
 import sys
+import signal
 import getpass
-if sys.version_info < (3,):
+from base64 import b64encode
+if sys.version_info >= (3,):
+    import pickle
+else:
+    import cPickle as pickle
     input = raw_input
 
+
 def ask():
+    '''
+    Ask login credentials to the user.
+    '''
     print('Logging in to the IRCCloud server\n', file=sys.stderr)
 
     # Get email address
@@ -29,3 +38,17 @@ def ask():
 
     print(file=sys.stderr)
     return (email, password)
+
+
+if __name__ == '__main__':
+    # Consider SIGINT as a goodbye signal
+    handler = lambda sig, frame: (print('\n\nGoodbye!', file=sys.stderr), sys.exit(0))
+    signal.signal(signal.SIGINT, handler)
+
+    # Ask the credentials
+    data = ask()
+
+    # Serialize the data
+    serialized = b64encode(pickle.dumps(data, 2)).decode()
+    print(serialized)
+    print('\x1b[34mPassing the data to the original process\x1b[0m', file=sys.stderr)
