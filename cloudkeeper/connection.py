99ed3a960c1e934b5dc91f65228daddfd85f12a1
Hyeon Kim <simnalamburt@gmail.com>
2018-11-01 20:51:59 +0900

    Refactoring

--- cloudkeeper/connection.py
+++ cloudkeeper/connection.py
@@ -4,6 +4,7 @@ import json
 import time
 import random
 import string
+import logging
 from threading import Thread
 
 import requests
@@ -16,53 +17,71 @@ from websocket import create_connection
 URL_LOGIN = 'https://www.irccloud.com/chat/login'
 URL_FORMAUTH = 'https://www.irccloud.com/chat/auth-formtoken'
 URL_ORIGIN = 'https://www.irccloud.com'
-URL_WEBSOCKET = 'wss://api.irccloud.com/websocket/' + random.choice(string.digits)
+
+logger = logging.getLogger(__name__)
 
 
-def auth(user_email, user_password):
+def auth(email, password):
+    logger.info('Starting authentication')
+
     # Retrieve a CSRF token
     resp = requests.post(URL_FORMAUTH, headers={'content-length': '0'})
-    token = resp.json()['token']
+    msg = resp.json()
+    if msg.get('success') is not True or 'token' not in msg:
+        return None
+    token = msg['token']
 
     # Retrieve a session key
-    data = {'email': user_email, 'password': user_password, 'token': token}
+    data = {'email': email, 'password': password, 'token': token}
     headers = {'x-auth-formtoken': token}
     resp = requests.post(URL_LOGIN, data=data, headers=headers)
-    session = json.loads(resp.text).get('session')
+    msg = resp.json()
+    if msg.get('success') is not True or 'session' not in msg:
+        return None
+    session = msg['session']
 
-    # Return session
-    return session
-
-
-def check(context, timeout):
-    while True:
-        time.sleep(5)
-        diff = time.time() - context['last']
-        if diff <= timeout or context['last'] == 0:
-            continue
-
-        # TODO: Is it connection timeout? Why this happens?
-        if context['socket'] is not None:
-            context['socket'].close()
-            # TODO: Shall we raise an Exception in here?
-        return
+    logger.info('Authentication completed: {}'.format(session))
+    url = 'wss://{}{}?exclude_archives=1'.format(
+        msg.get('websocket_host', 'api.irccloud.com'),
+        msg.get('websocket_path', '/websocket/{}'.format(random.choice(string.digits))),
+    )
+    return session, url
 
 
-def connect(session, timeout=120.0, on_succeeded=None, on_disconnect=None):
+def check(context, timeout, check_period):
+    while time.time() - context['last'] <= timeout:
+        time.sleep(check_period)
+
+    # TODO: Investigate when and why this happens
+    logger.debug(
+        ('No message on the socket for {} seconds. ' +
+         'Disconnecting the socket').format(timeout)
+    )
+    if context['socket'] is not None:
+        context['socket'].close()
+
+
+def connect(session, websocket_url, timeout=120.0, check_period=5.0):
+    socket = create_connection(
+        websocket_url,
+        header=['Cookie: session={}'.format(session)],
+        origin=URL_ORIGIN,
+    )
+    logger.info('Connection created at {}'.format(websocket_url))
+
+    # Send authentication message
+    socket.send(json.dumps({
+        'cookie': session,
+        '_reqid': 1,
+        '_method': 'auth',
+    }))
+
     # 여러 스레드가 동시에 접근할 정보들
     context = {
-        'last': 0,
-        'socket': create_connection(
-            URL_WEBSOCKET,
-            header=['Cookie: session={}'.format(session)],
-            origin=URL_ORIGIN
-        ),
+        'last': time.time(),
+        'socket': socket,
     }
-
-    if on_succeeded is not None:
-        on_succeeded()
-
-    thread = Thread(target=check, args=(context, timeout), daemon=True)
+    thread = Thread(target=check, args=(context, timeout, check_period), daemon=True)
     thread.start()
 
     while True:
@@ -72,7 +91,15 @@ def connect(session, timeout=120.0, on_succeeded=None, on_disconnect=None):
         context['last'] = time.time()
 
         msg = json.loads(payload)
-        # TODO: Do something with msg
+        logger.debug(msg)
 
-    if on_disconnect is not None:
-        on_disconnect()
+        if msg.get('type') == 'oob_include':
+            url = msg['url']
+            headers = {
+                'Cookie': 'session={}'.format(session),
+                'Accept-Encoding': 'gzip',
+            }
+            resp = requests.get(URL_ORIGIN + url, headers=headers)
+            logger.debug('Backlog retrieved, status code: {}'.format(resp.status_code))
+
+    logger.warning('Disconnected from the network')
--- cloudkeeper/mainloop.py
+++ cloudkeeper/mainloop.py
@@ -9,9 +9,11 @@ import logging
 
 from .connection import auth, connect
 
+logger = logging.getLogger(__name__)
+
 
 def sigint_handler(sig, frame):
-    logging.info('Recieved SIGINT, turning off the process. Goodbye!')
+    logger.info('Recieved SIGINT, turning off the process. Goodbye!')
     sys.exit(0)
 
 
@@ -32,32 +34,24 @@ def mainloop(delay=30):
     email = os.environ.get('CLOUDKEEPER_EMAIL')
     password = os.environ.get('CLOUDKEEPER_PASSWORD')
     if email is None:
-        logging.error('Please set CLOUDKEEPER_EMAIL environment variable')
+        logger.error('Please set CLOUDKEEPER_EMAIL environment variable')
         sys.exit(1)
     if password is None:
-        logging.error('Please set CLOUDKEEPER_PASSWORD environment variable')
+        logger.error('Please set CLOUDKEEPER_PASSWORD environment variable')
         sys.exit(1)
 
     # Try to connect to the server infinitely
     while True:
-        logging.info('Starting authentication')
-        sys.stdout.flush()
-        session = auth(email, password)
+        session, url = auth(email, password)
         if session is None:
-            logging.error('Authentication failed')
+            logger.error('Authentication failed')
             sys.exit(1)
-        logging.info('Authentication completed')
 
         try:
-            connect(
-                session,
-                on_succeeded=lambda: logging.info('Connection created'),
-                on_disconnect=lambda: logging.warning('Disconnected from the network'),
-            )
+            connect(session, url)
         except SystemExit:
             raise
         except Exception:
-            logging.error('Unhandled exception occurred:')
-            logging.error(traceback.format_exc())
-            logging.error('Disconnected, reconnecting in {} seconds'.format(delay))
+            logger.error('Unhandled exception occurred:\n{}'.format(traceback.format_exc()))
+            logger.error('Disconnected, reconnecting in {} seconds'.format(delay))
             time.sleep(delay)
